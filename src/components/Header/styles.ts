9e00dde1c4228e7fc8a181baa8fc38a3145bbe97
Priscila <pri.wagner.silva@gmail.com>
2025-07-28 19:01:16 +0100

    fonte robot adicionada

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,34 +1,6 @@
 import styled from "styled-components";
 import { colors } from "../../styles";
 
-export const HeaderBar = styled.header`
-  background-color: ${colors.aqua};
-  color: ${colors.white};
-  height: 360px;
-  margin-top: 24px;
-  display: grid;
-  place-items: center;
+export const HeaderBar = styled.header``;
 
-  img {
-    height: 150px;
-  }
-  p {
-    font-size: 16px;
-    margin-left: 16px;
-  }
-`;
-
-export const LogoIcon = styled.img`
-  height: 48px;
-  margin-left: 16px;
-  animation: wag 1s infinite alternate ease-in-out;
-
-  @keyframes wag {
-    0% {
-      transform: rotate(-5deg);
-    }
-    100% {
-      transform: rotate(5deg);
-    }
-  }
-`;
+export const LogoIcon = styled.img``;
